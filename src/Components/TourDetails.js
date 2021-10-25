04ec844a6bfcf8869825378290a6cc5ffbef3ab3
Your Name <adeyeye17@gmail.com>
2021-10-26 00:09:13 +0200

    currently working on the css style

--- src/Components/TourDetails.js
+++ src/Components/TourDetails.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 function TourDetails({id, image, price, name, info}) {
     return (
-        <article>
-            <h1> ghajvsdgzsfhs</h1>
+        <article className="each-Tour">
+             <img src={image} alt={name} /> 
+            <footer className="footer-tour" >
+            <div>
+            <h4> {name}</h4>
+            <h4> {price}</h4>
+            </div>   
+            <p> {info}</p>
+            <button>
+                not interested
+            </button>
+             </footer>
         </article>
     )
 }
--- src/Components/Tours.js
+++ src/Components/Tours.js
@@ -6,14 +6,19 @@ export default function Tours({tours}) {
     return (
              
 <section>
+ <div className="">
+     
+    {tours.map((tourList) => {
+        return <TourDetails key={tourList.id}  {...tourList} /> 
+        //spread operator was used to get an iterable (e.g an array) and expands it into individual elements. So in this case we used the spread operator to get the data into TourDetails....  
+        
+    } )}
 
- <div className="title">
-    <h2> ours tours</h2>
- </div>
-    {tours.map((tourList)=> <TourDetails key = {tourList.id} {...tours}> </TourDetails>
+
+</div>
+</section>
 
 )}
 
-</section>
-    )
-}
+
+  
