462c33abaaa864e6430cdcf0fc44aedadcc092fb
Your Name <adeyeye17@gmail.com>
2021-10-26 02:09:07 +0200

    I implemented the ream more and show less button

--- src/Components/TourDetails.js
+++ src/Components/TourDetails.js
@@ -1,16 +1,29 @@
-
+import React,{useState} from "react"
 
 function TourDetails({id, name, image, price, info}) {
+    const [readmore, setReadMore] = useState(false)
+
+    const Readmebtn =()=>{
+        setReadMore(!readmore)
+    }
+
    return(
     <article>
+     
         <div className="each-Tour">
         <img src={image} alt= {name} />
         <footer className="footer-tour">
         <div className="title-tour">
             <h4>{name}</h4>
-            <h4 className="title-price-tour" >{price}</h4>
+            <h4 className="title-price-tour">{`$${price}`}</h4> 
         </div>
-        <p>{info}</p>
+        <p>
+        {readmore ? `${info}  ` : `${info.substring(0,200)} ... `}
+        <button onClick={Readmebtn} className="read-more-btn">
+            {readmore ? "show less ":  "read more"  }
+        </button>
+            
+         </p>
         <button className="delete-btn"> not interested</button>
         </footer>
         </div>
